a500d6e63bf0cf0d9ad059c9c41ce4d5bf9f3eda
Christy Waickem <waickem@gmail.com>
2017-04-12 17:39:28 +0300

    Base Template

---
+++ src/main/java/md/curs/servlets/Home.java
@@ -0,0 +1,24 @@
+package md.curs.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Home
+ */
+@WebServlet (urlPatterns = {"/home"})
+public class Home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+	}
+	
+}
